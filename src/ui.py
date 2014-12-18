e1bbcbc1e9f629fda550a4802642dce59d7db26f
Yury Khrustalev <yuri.khrustalyov@gmail.com>
2014-12-18 23:47:25 +0000

    ask user when exiting application

--- src/ui.py
+++ src/ui.py
@@ -14,3 +14,10 @@ class MainWindow(QtGui.QWidget):
     self.setWindowTitle('%s - %s' % ("Jaylen", "0.1.0"))
     self.setWindowIcon(QtGui.QIcon('resources/main.png'))
     self.show()
+
+  def closeEvent(self, event):
+    reply = QtGui.QMessageBox().question(self, "Confirm application exit", "Do you really want to exit?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+    if reply == QtGui.QMessageBox.Yes:
+      event.accept()
+    else:
+      event.ignore()
