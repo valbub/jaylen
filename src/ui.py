7fe1e28ff5e87e4fe23d5c57c8ede02b7b6eab13
Yury Khrustalev <yuri.khrustalyov@gmail.com>
2014-12-18 23:59:11 +0000

    main menu and status bar

--- src/ui.py
+++ src/ui.py
@@ -3,21 +3,41 @@
 from PyQt4 import QtGui
 
 
-class MainWindow(QtGui.QWidget):
+class MainWindow(QtGui.QMainWindow):
 
   def __init__(self):
     super(MainWindow, self).__init__()
-    self.initUI()
+    self.init()
 
-  def initUI(self):
-    self.setGeometry(100, 200, 800, 400)
+  def init(self):
+    self.resize(800, 480)
+    self.center()
     self.setWindowTitle('%s - %s' % ("Jaylen", "0.1.0"))
     self.setWindowIcon(QtGui.QIcon('resources/main.png'))
+    self.statusBar().showMessage('ready')
+    self.buildMainMenu()
     self.show()
 
   def closeEvent(self, event):
-    reply = QtGui.QMessageBox().question(self, "Confirm application exit", "Do you really want to exit?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+    buttons = QtGui.QMessageBox.Yes | QtGui.QMessageBox.No
+    default = QtGui.QMessageBox.No
+    box = QtGui.QMessageBox()
+    reply = box.question(self, "Confirm application exit", "Do you really want to exit?", buttons, default)
     if reply == QtGui.QMessageBox.Yes:
       event.accept()
     else:
       event.ignore()
+
+  def center(self):
+    frameGeometry = self.frameGeometry()
+    frameGeometry.moveCenter(QtGui.QDesktopWidget().availableGeometry().center())
+    self.move(frameGeometry.topLeft())
+
+  def buildMainMenu(self):
+    exitAction = QtGui.QAction('&Exit', self)
+    exitAction.setShortcut('Ctrl+Q')
+    exitAction.setStatusTip('Exit application')
+    exitAction.triggered.connect(QtGui.qApp.quit)
+    menuBar = self.menuBar()
+    fileMenu = menuBar.addMenu('&File')
+    fileMenu.addAction(exitAction)
